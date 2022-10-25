0106eeabbc555dbd9defc7346a7ad3d5e8935be4
Bianca Carvalho <carvalho.bianca@hotmail.com>
2022-10-25 21:09:56 +0100

    page foot and head created

---
+++ head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clínica de Nutrição</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="container">
+    <header>
+        <h1 class="text_align_center">Clínica de Nutrição</h2>
+            <div>
+                <nav class="menu">
+                        <ul>
+                            <li>
+                                <a href="index.php">Home</a> |
+                            </li>
+                            <li>
+                                <a href="page_cadastros.php">Lista de Cadastros</a> |
+                            </li>
+                            <li>
+                                <a href="page_imc.php">Lista de IMC</a>
+                            </li>
+                        </ul>
+                </nav>
+            </div>
+   <div>
+    </header>
+    
+    
